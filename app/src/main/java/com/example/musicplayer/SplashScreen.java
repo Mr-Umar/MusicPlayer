660ca1a4c61dcfa981dc8b455571b22e19fca240
Mr-Umar <inoxentumar01@gmail.com>
2021-08-22 21:17:08 +0500

    update

--- app/src/main/java/com/example/musicplayer/SplashScreen.java
+++ app/src/main/java/com/example/musicplayer/SplashScreen.java
@@ -1,17 +1,14 @@
 package com.example.musicplayer;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.view.WindowManager;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity
 
 {
-
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
